1cd3b3e993d17731d4b5104702b13d425399e9d2
acook14 <acook14@luc.edu>
2023-11-13 16:40:22 -0600

    List of a few possible models

--- wine-quality.py
+++ wine-quality.py
@@ -1,3 +1,10 @@
 import numpy as np
 import pandas as pd
 
+""" 
+    List of models to write:
+        - Regression 
+        - Good vs. Bad Classification (ex. set <7 good, >7 bad)
+        - Exact Quality Classification? (possible classifications in dataset: 3, 4, 5, 6, 7, 8)
+
+"""
